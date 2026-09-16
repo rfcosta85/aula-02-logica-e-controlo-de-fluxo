aa70696c990a312954ae2c76caaaddbaf3fd0a6d
Rodrigo Fernandes <rfcosta85@gmail.com>
2026-09-16 11:39:09 +0100

    exemplo 01 - if Simples

---
+++ 01-if.js
@@ -0,0 +1,34 @@
+/* Exemplo 01 */
+
+let diaSemana = new Date();
+
+numeroSemana = diaSemana.getDay();
+
+
+if(numeroSemana == 1) {
+    console.log("Segunda Feira");
+}
+
+if (numeroSemana == 2) {
+    console.log("Terça Feira");
+}
+
+if(numeroSemana == 3) {
+    console.log("Quarta Feira");
+}
+
+if(numeroSemana == 4) {
+    console.log("Quinta Feira");
+}
+
+if(numeroSemana == 5) {
+    console.log("Sexta Feira");
+}
+
+if(numeroSemana == 6) {
+    console.log("Sábado");
+}
+
+if(numeroSemana == 7) {
+    console.log("Domingo");
+}
